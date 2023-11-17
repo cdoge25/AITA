5cbfa1bbd54471ed6689aa117364f29b3151a71e
cdoge25 <hainguyenducthien@gmail.com>
2023-11-17 18:11:04 +0700

    init some files

---
+++ main.py
@@ -0,0 +1,14 @@
+#from aita_clean:
+#100 before last: 1580540276 - 2020-02-01 07:00:00
+#last known datetime: 1580577998 - 2020-02-01 17:00:00
+
+#which is 100 posts every 10 hours
+
+#now start from 1580580000 - 2020-02-01 18:00:00
+#end epoch - start epoch = 36000s
+start_epoch = 1
+end_epoch = start_epoch + 36000
+subreddit_name = "AmItheAsshole"
+example_api_url = f"https://api.pullpush.io/reddit/search/submission/?subreddit={subreddit_name}&after={start_epoch}6&before={end_epoch}"
+
+#
